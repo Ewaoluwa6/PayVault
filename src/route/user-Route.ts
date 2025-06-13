6a15f7487919c31200279521ea9439735ed08900
Moni Ayo Saka <msaka@parthianpartnersng.com>
2025-06-13 18:52:39 +0100

    Routes and controller Moni-Ayo

---
+++ src/route/user-Route.ts
@@ -0,0 +1,13 @@
+// src/routes/userRoute.ts
+import express, { Router } from 'express'
+import Users from '../controller/userController'
+
+const router: Router = express.Router()
+const userController = new Users()
+
+const {transferVault, convertCurrency, withdrawVault} = new Users()
+router.post("/wallet/withdraw", withdrawVault)
+router.post("/wallet/convert", convertCurrency)
+router.post("/wallet/transfer", transferVault)
+
+export default router
