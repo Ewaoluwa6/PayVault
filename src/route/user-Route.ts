bf5888cfef2045ccf3c8ca8e397eb66284403d80
Wisdom Enyioha <wenyioha@parthianpartnersng.com>
2025-06-18 13:36:47 +0100

    completed create and login user implementation

--- src/route/user-Route.ts
+++ src/route/user-Route.ts
@@ -1,11 +1,18 @@
 // src/routes/userRoute.ts
+
 import express, { Router } from 'express'
 import Users from '../controller/userController'
+
+
 
 const router: Router = express.Router()
 const userController = new Users()
 
-const {transferVault, convertCurrency, withdrawVault, getDetails, updateProfile,createDetails,fundAccount} = new Users()
+const {
+    transferVault, convertCurrency, withdrawVault,
+    getDetails, updateProfile,createDetails,fundAccount,
+    create, login, changePassword}
+     = new Users()
 router.post("/wallet/withdraw", withdrawVault)
 router.post("/wallet/convert", convertCurrency)
 router.post("/wallet/transfer", transferVault)
@@ -17,4 +24,17 @@ router.patch("/auth", updateProfile)
 router.post("/wallet", createDetails)
 router.post("/wallet", fundAccount)
 
+router.post("/wallet/withdraw", withdrawVault)
+router.post("/wallet/convert", convertCurrency)
+router.post("/wallet/transfer", transferVault)
+
+router.post('/create', create)
+router.post("/login", login)
+router.post("/change-password", changePassword)
+
 export default router
+
+
+
+
+
